ea4cdde68bce27b9b6d51a755aa17182dd3d9ac3
Om Yeole <omyeole01@gmail.com>
2024-12-11 18:42:27 +0530

    Created file references_practice.cpp and practiced about references

---
+++ Essentials/references_practice.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a = 10;
+    int &r = a;
+
+    cout<<a<<endl;
+    cout<<r<<endl;
+
+    r++;
+    cout<<a<<endl;
+    cout<<r<<endl;
+
+    a++;
+    cout<<a<<endl;
+    cout<<r<<endl;
+
+    cout<<&a<<endl;
+    cout<<&r<<endl;
+
+    return 0;
+}
