7838b1946e96633abd2e5b63c28c7fcfad0c48fe
Om Yeole <omyeole01@gmail.com>
2024-12-23 16:34:40 +0530

    Practiced about functions in file functions_practice.cpp

---
+++ Essentials/functions_practice.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int add(int a, int b){
+    return a+b;
+}
+
+int main(){
+    int a = 10, b = 20;
+    cout<<add(a, b)<<endl;
+    
+
+    return 0;
+}
